0452978037086045a77c73d887a6e6353a409cd4
JosephWondimagnehu <joseph.wondimagnehu@stu.ocsb.ca.>
2025-04-19 11:30:20 -0400

    Fixed error and changed sets to arrays in letterClassifier.cpp

--- letterClassifier.cpp
+++ letterClassifier.cpp
@@ -11,26 +11,27 @@ phonetic symbols and examples if desired. */
 /* Include the iostream library and others for
 extra functionalities used in this program. */
 #include <iostream>
-#include <set>
+#include <limits>
+
 
 // Runs the main function.
 int main() {
-    /* Initialize constant sets for input error 
-    detections and displaying information. */
-    const std::set<char> ENGLISH_LETTERS = {
+    /* Initialize constant arrays for input 
+    error detections and displaying information. */
+    const char ENGLISH_LETTERS[] = {
         'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
         'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
         's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
     };
-    const std::set<std::string> PHONETIC_SYMBOLS = {
+    const std::string PHONETIC_SYMBOLS[] = {
         "/ˈeɪ/", "/ˈbiː/", "/ˈsiː/", "/ˈdiː/", "/ˈiː/",
         "/ˈɛf/", "/ˈdʒiː/", "/eɪtʃ/", "/ˈaɪ/", "/ˈdʒeɪ/",
         "/ˈkeɪ/", "/ˈɛl/", "/ˈɛm/", "/ˈɛn/", "/ˈoʊ/", 
         "/ˈpiː/", "/ˈkjuː/", "/ˈɑːr/", "/ˈɛs/", "/ˈtiː/",
         "/ˈjuː/", "/ˈviː/", "/ˈdʌb.əlˈjuː/", "/ˈɛks/",
-        "/ˈwaɪ/", "/ˈziː/ or /ˈzɛd/"
-    },
-    EXAMPLE_WORDS = {
+        "/ˈwaɪ/", "/ˈziː/ or /ˈzɛd/" 
+    };
+    const std::string EXAMPLE_WORDS[] = {
         "ate", "bean", "cow", "deed", "ever",
         "friend", "gene", "house", "ice", "jam",
         "kangaroo", "lid", "make", "new", "one",
@@ -50,11 +51,24 @@ int main() {
     WHITE = "\033[0m",
     BOLD = "\033[1m";
 
+    // Declare the string variables for user input.
+    std::string userLetter, userDisplayType;
+    
+    // Declare the integer variables for user input.
+    int showPhoneticSymbolInt, showPhoneticExampleInt;
+
     // Display a greeting message to the user.
     std::cout << "\n" << LIGHT_PURPLE << "Hello, user! This is a program "
     << "designed to determine whether " << "an entered\n" << "letter "
     << "from the English alphabet is a consonant, vowel, or\n"
     << "semivowel, with extra examples and modes for more information."
     << WHITE << "\n";
-    
+
+    // Construct an infinite while loop for the letter input.
+    /* while (true) {
+        // Ask the user to enter a letter.
+        std::cout << DARK_GRAY << "Enter a letter from a-z or A-Z:" << WHITE 
+        << "\n" << "";
+        
+    } */
 }
